6d37a9dfa7eb783e781cbfdab507d67a34bc565c
Mike Ottinger <mikeottinger@gmail.com>
2012-12-31 14:31:35 -0800

    moved to tomcat7

--- site-cookbooks/mike_tomcat/recipes/default.rb
+++ site-cookbooks/mike_tomcat/recipes/default.rb
@@ -1,15 +1,15 @@
-include_recipe "tomcat::default"
+include_recipe "tomcat7::default"
 
-cookbook_file "#{node[:tomcat][:webapp_dir]}/sample.war" do
+cookbook_file "#{node[:tomcat7][:home]}/sample.war" do
   source "sample.war"
   mode 0755
-  owner node[:tomcat][:user]
-  group node[:tomcat][:group]
+  owner node[:tomcat7][:user]
+  group node[:tomcat7][:group]
 end
 
 directory node[:tomcat][:ssl][:keystore_directory] do
-  owner node[:tomcat][:user]
-  group node[:tomcat][:group]
+  owner node[:tomcat7][:user]
+  group node[:tomcat7][:group]
   mode 00700
   action :create
   only_if { node[:tomcat][:ssl][:enabled] }
@@ -24,15 +24,15 @@ mike_tomcat_ssl node[:tomcat][:ssl][:alias] do
   o node[:tomcat][:ssl][:o]
   c node[:tomcat][:ssl][:c]
   validity node[:tomcat][:ssl][:validity]
-  user node[:tomcat][:user]
+  user node[:tomcat7][:user]
   
   action node[:tomcat][:ssl][:enabled] ? :enable : :disable
 end
 
-template "/etc/tomcat6/server.xml" do
+template "#{node[:tomcat7][:target]}/tomcat/conf/server.xml" do
   source "server.xml.erb"
   owner "root"
   group "root"
   mode "0644"
-  notifies :restart, resources(:service => "tomcat")
+  notifies :restart, resources(:service => "tomcat7")
 end
