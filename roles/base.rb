267c0f6156b05adeed6fac1080208e686f3c7575
Mike Ottinger <mikeottinger@gmail.com>
2013-01-04 18:04:46 -0800

    added the rsync recipe

--- roles/base.rb
+++ roles/base.rb
@@ -1,3 +1,3 @@
 name "roles"
 description "Base recipes for provisioning a server"
-run_list("recipe[yum]", "recipe[vim]", "recipe[ntp]", "recipe[mike_iptables]", "role[tomcat]", "role[database]", "recipe[playground]")
+run_list("recipe[yum]", "recipe[vim]", "recipe[rsync]", "recipe[ntp]", "recipe[mike_iptables]", "role[tomcat]", "role[database]", "recipe[playground]")
