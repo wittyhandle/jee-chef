9f8c4d147cfcbc561ad3343c25957d6b3683c0f3
Mike Ottinger <mikeottinger@gmail.com>
2013-01-06 13:14:41 -0800

    add the database cookbook and use it to add tables

--- site-cookbooks/playground/recipes/default.rb
+++ site-cookbooks/playground/recipes/default.rb
@@ -1,6 +1,15 @@
+include_recipe "database::mysql"
+
+mysql_connection_info = {:host => "localhost", :username => 'root', :password => node[:playground][:password]}
+
 template "/opt/connection.properties" do
   source "connection.properties.erb"
   owner "root"
   group "root"
   mode 00444
+end
+
+mysql_database 'cdgd' do
+  connection mysql_connection_info
+  action :create
 end
